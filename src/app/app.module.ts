a0d3a931f546a66cccfd4e7e60e2101d4783d58d
sathish <sathish12.vs@gmail.com>
2023-04-24 17:03:19 +0530

    v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyBarChartComponent } from './my-bar-chart/my-bar-chart.component';
-import { NgChartsModule } from 'ng2-charts';
+
 import { MyDoughnutChartComponent } from './my-doughnut-chart/my-doughnut-chart.component';
 
 
 import * as CanvasJSAngularChart from '../assets/canvasjs.angular.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { NgChartsModule } from 'ng2-charts';
 var CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
 
 
--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -10,10 +10,10 @@ export class MyBarChartComponent implements OnInit {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
-  public barChartData = [
+   barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+   barChartType = 'bar';
+   barChartLegend = true;
+   barChartData = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
   ];
